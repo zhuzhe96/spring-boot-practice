237b6f0a1dfb5b6e0590b48ef3d9ccabfc8e7b3e
zhuzhe <zhuzhe_96@163.com>
2023-05-27 11:32:57 +0800

    feature: 加一个新的数据校验的模块

---
+++ validating-input/src/main/java/com/zhuzhe/validatinginput/ValidatingInputApplication.java
@@ -0,0 +1,13 @@
+package com.zhuzhe.validatinginput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingInputApplication.class, args);
+	}
+
+}
---
+++ validating-input/src/test/java/com/zhuzhe/validatinginput/ValidatingInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuzhe.validatinginput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
