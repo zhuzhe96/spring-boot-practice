df65bc888858643a1d840de5ef2075a5fd8f8202
zhuzhe <zhuzhe_96@163.com>
2023-06-06 14:27:23 +0800

    feature: 加一个由Security实现的RBAC初始模块

---
+++ security-rbac/src/main/java/com/zhuzhe/securityrbac/SecurityRbacApplication.java
@@ -0,0 +1,13 @@
+package com.zhuzhe.securityrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRbacApplication.class, args);
+	}
+
+}
---
+++ security-rbac/src/test/java/com/zhuzhe/securityrbac/SecurityRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuzhe.securityrbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
